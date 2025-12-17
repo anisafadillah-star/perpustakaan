92cdfe5247ad45b696bb44914f3e5976032a0631
anisafadillah-star <anisafadillahmaulana31@gmail.com>
2025-12-17 14:14:44 +0700

    update sql perpus

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 10, 2025 at 07:41 AM
+-- Generation Time: Dec 17, 2025 at 07:12 AM
 -- Server version: 12.0.2-MariaDB-log
 -- PHP Version: 8.3.26
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `perpustakaan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kode_buku` varchar(50) NOT NULL,
+  `judul` varchar(150) NOT NULL,
+  `penulis` varchar(100) DEFAULT NULL,
+  `penerbit` varchar(100) DEFAULT NULL,
+  `tahun_terbit` year(4) DEFAULT NULL,
+  `stok_total` int(11) NOT NULL DEFAULT 0,
+  `stok_tersedia` int(11) NOT NULL DEFAULT 0,
+  `rak_lokasi` varchar(50) DEFAULT NULL,
+  `status` enum('tersedia','dipinjam','rusak','hilang') NOT NULL DEFAULT 'tersedia',
+  `foto` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `kode_buku`, `judul`, `penulis`, `penerbit`, `tahun_terbit`, `stok_total`, `stok_tersedia`, `rak_lokasi`, `status`, `foto`, `created_at`, `updated_at`) VALUES
+(1, 'BK001', 'Laskar Pelangi', 'Andrea Hirata', 'Bentang Pustaka', '2005', 8, 8, 'A1', 'tersedia', '1762142255_f27371e1a7a02f556b36.jpeg', NULL, NULL),
+(2, 'BK002', 'Bumi Manusia', 'Pramoedya Ananta Toer', 'Hasta Mitra', '1980', 5, 2, 'A2', 'dipinjam', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -99,6 +129,21 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `laporan`
+--
+
+CREATE TABLE `laporan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `periode` date NOT NULL,
+  `jumlah_buku` int(11) NOT NULL,
+  `total_denda` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -116,7 +161,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2025_12_05_080506_add_role_to_users_table', 2);
+(4, '2025_12_15_144442_create_buku_table', 1),
+(5, '2025_12_15_144458_create_peminjaman_table', 1),
+(6, '2025_12_16_083416_create_pengembalian_table', 1),
+(7, '2025_12_16_083452_create_petugas_table', 1),
+(8, '2025_12_16_083511_create_pengunjung_table', 1),
+(9, '2025_12_16_083519_create_laporan_table', 1);
 
 -- --------------------------------------------------------
 
@@ -128,6 +178,69 @@ CREATE TABLE `password_reset_tokens` (
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` bigint(20) UNSIGNED NOT NULL,
+  `tgl_peminjaman` date NOT NULL,
+  `tgl_deadline` date NOT NULL,
+  `id_user` bigint(20) UNSIGNED DEFAULT NULL,
+  `id_buku` bigint(20) UNSIGNED DEFAULT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'dipinjam',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` bigint(20) UNSIGNED NOT NULL,
+  `id_peminjaman` bigint(20) UNSIGNED NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `denda` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengunjung`
+--
+
+CREATE TABLE `pengunjung` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `jenis_pengunjung` enum('siswa','guru','umum') NOT NULL,
+  `nis_nip` varchar(255) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -145,14 +258,6 @@ CREATE TABLE `sessions` (
   `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `sessions`
---
-
-INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('4SkF3IqarNaq8NXzaO1OFmuUVwZ8XSEOsSATklBl', 3, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoieVdLWlEwZElBWUNjcGZPam1GYzJwb2g1SW5jQjlOZTF5Z21veGlpWSI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQvc3RhZmYiO3M6NToicm91dGUiO3M6MTU6ImRhc2hib2FyZC5zdGFmZiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjM7fQ==', 1764927206),
-('t2Ffyx61PmORvQCVOhltOs74ssB1Yd5BQpmr7FmD', 3, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUlFzSVZqdHdTSDRoaTVGbzZoN0NOSHkxWjNTYmNEd2RuUnF0WHNrSCI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7czo3OiJsYW5kaW5nIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mzt9', 1765352343);
-
 -- --------------------------------------------------------
 
 --
@@ -161,28 +266,37 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
+  `role` enum('admin','petugas','siswa','guru') NOT NULL DEFAULT 'siswa',
+  `foto` varchar(255) NOT NULL DEFAULT 'default.png',
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
-(1, 'Test User', 'test@gmail.com', '2025-12-05 01:25:36', '$2y$12$ojgXRfo0zlqHYMAZ4xp6SOWvQWHq0tJsTJf.KJJcPHfK.brYHzcvW', 'vtlgEFj0Zm', '2025-12-05 01:25:38', '2025-12-05 01:25:38', NULL),
-(2, 'Admin', 'admin@gmail.com', NULL, '$2y$12$F6CWwGOmSTRqPVOgiXzeb.2s93uNuFovMAJ7nJ/RQmXvG80nthQ3O', NULL, '2025-12-05 01:25:38', '2025-12-05 01:25:38', 'admin'),
-(3, 'Petugas', 'petugas@gmail.com', NULL, '$2y$12$D1l0zoe1XCFYsNoGasKfD.l5hGFOfQWopBeeQiVpzFjDzulcwq6rS', NULL, '2025-12-05 01:25:39', '2025-12-05 01:25:39', 'petugas');
+INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `role`, `foto`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Anisa F', 'anisa', 'anisa@gmail.com', '$2y$12$fYyKF9ltWdWkFZQmXz2aXeaYa9rEl6rxQ2J4guLaaSn//CcI0Whj.', 'siswa', 'default.png', NULL, '2025-12-16 02:15:39', NULL),
+(2, 'Nurul A', 'nurul', 'nurul@gmail.com', '$2y$12$JyBxDJMqUA..0o/Ry54xhOAyq4ENwZnpGoG0W2BXmNzJMLNj3/is.', 'guru', 'default.png', NULL, '2025-12-16 02:15:39', NULL),
+(3, 'petugas Perpus', 'petugas', 'petugas@perpus.com', '$2y$12$uqkGfVzeLM6PnOXtRPreC./cSOVg/pbLEEy9XLpPCEfBt0uW7GcTu', 'petugas', 'default.png', NULL, '2025-12-16 02:15:39', NULL),
+(4, 'Admin Perpus', 'admin', 'admin@perpus.com', '$2y$12$i14c9BdrMzJ5Cf7FVtBMpeEL0wykusCZQVuGl3pQN6OXnDzkFShFO', 'admin', 'default.png', NULL, '2025-12-16 02:15:40', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `buku_kode_buku_unique` (`kode_buku`);
 
 --
 -- Indexes for table `cache`
@@ -217,6 +331,12 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `laporan`
+--
+ALTER TABLE `laporan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -229,11 +349,38 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD KEY `peminjaman_id_user_foreign` (`id_user`),
+  ADD KEY `peminjaman_id_buku_foreign` (`id_buku`);
+
+--
+-- Indexes for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`),
+  ADD KEY `pengembalian_id_peminjaman_foreign` (`id_peminjaman`);
+
+--
+-- Indexes for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
 -- Indexes for table `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_user_id_foreign` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
@@ -241,11 +388,18 @@ ALTER TABLE `sessions`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -260,16 +414,69 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `laporan`
+--
+ALTER TABLE `laporan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_id_buku_foreign` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `peminjaman_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD CONSTRAINT `pengembalian_id_peminjaman_foreign` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
